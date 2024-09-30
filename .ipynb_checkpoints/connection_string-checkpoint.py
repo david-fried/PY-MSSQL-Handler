6040f15bdf2954b7878fb4b0607aa4e9c06f6738
David Fried <david.d.fried@gmail.com>
2024-09-29 20:04:55 -0400

    adds .gitignore file and untracks jupyter notebook artifacts

--- .ipynb_checkpoints/connection_string-checkpoint.py
+++
@@ -1,8 +0,0 @@
-class ConnectionString:
-	
-	def __init__(self):
-		# Replace with own connection strings. Examples below are formatted for MS SQL Server.
-		self.prod = r'Driver={ODBC Driver 17 for SQL Server}; Server=XXXXXXX; Database=<ProdDatabaseName>; Trusted_Connection=yes;'
-		self.dev =  r'Driver={ODBC Driver 17 for SQL Server}; Server=XXXXXXX; Database=<DevDatabasename>; Trusted_Connection=yes;'
-		self.qa =  r'Driver={ODBC Driver 17 for SQL Server}; Server=XXXXXXX; Database=<QADatabaseName>; Trusted_Connection=yes;'
-		self.local = r'Driver={ODBC Driver 17 for SQL Server}; Server=(localDB)\MSSQLLocalDB; Database=<MyLocalDatabaseName>;'
